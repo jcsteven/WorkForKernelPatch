91b62fac24f0d8e8d2a87259592724d1af76e201
jcsteven <jcsteven.yu@gmail.com>
2018-09-13 16:16:44 +0800

    The fisrt check in

---
+++ 3.7.10-Kernel-patch.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+# -------------------------------------------------------
+# Automatic preparation script for rtk_openwrt 
+# JC Yu,     Novenber 26,2015
+# -------------------------------------------------------
+# IMPORTANT:
+#   When use: './<this script file>  '
+# -------------------------------------------------------
+TODAY=`date +"%Y-%m%d-%H%M"`
+PPWW=${PWD}
+WORK_NAME=KERNEL
+
+VER=3.7.10
+PNAME="patches"
+## if PNAME is "origin", you don't need to prepare folder
+## if PNAME is not "origin", you need to prepare PNAME-VER.tar.gz 
+
+KERNEL_NAME=linux-${VER}
+KERNEL_FILE=${KERNEL_NAME}.tar.xz
+
+if [[ "${PNAME}" == "origin" ]]; then
+  KERNEL_NAME_PATCH=${KERNEL_NAME}
+else
+  KERNEL_NAME_PATCH=${KERNEL_NAME}-${PNAME}
+fi
+
+KERNEL_DIR="./${WORK_NAME}/${KERNEL_NAME_PATCH}"
+
+PATCH_NAME=${PNAME}-${VER}
+PATCH_GZ_FILE="./patches-gz/${PATCH_NAME}.tar.gz"
+PATCH_DIR="${PPWW}/${WORK_NAME}/${PATCH_NAME}"
+PATCH_LIST="${PATCH_DIR}/series"  
+
+echo "PPWW=${PPWW}"
+echo "PWD=${PWD}"
+echo "KERNEL_NAME=${KERNEL_NAME}"
+echo "KERNEL_NAME_PATCH=${KERNEL_NAME_PATCH}"
+echo "KERNEL_FILE=${KERNEL_FILE}"
+echo "KERNEL_DIR=${KERNEL_DIR}"
+
+echo "PATCH_NAME=${PATCH_NAME}"
+echo "PATCH_GZ_FILE=${PATCH_GZ_FILE}"
+echo "PATCH_DIR=${PATCH_DIR}"
+echo "PATCH_LIST=${PATCH_LIST}"
+
+
+TO_PREPARE_WORK="y"
+if [[ "${TO_PREPARE_WORK}" == "y" ]]; then	
+	# Create Working DIR
+	[ -d $WORK_NAME ] || mkdir $WORK_NAME
+
+	if [ ! -f "./${WORK_NAME}/${KERNEL_FILE}" ]; then
+	   echo $LINENO "Missing ./${WORK_NAME}/${KERNEL_FILE}"
+	   echo $LINENO "Downloading ${KERNEL_FILE} form Interne"	   
+	   wget  https://www.kernel.org/pub/linux/kernel/v3.x/${KERNEL_FILE} -P ./${WORK_NAME}/
+    else
+	   echo $LINENO "Downloaded ${KERNEL_FILE} form Interne"
+	fi		
+
+fi
+
+TO_MAKE_DIR="y"
+if [[ "${TO_MAKE_DIR}" == "y" ]]; then	
+    echo $LINENO "Generating  ${KERNEL_DIR}"
+	if [ ! -f "./${WORK_NAME}/${KERNEL_FILE}" ]; then
+	   echo $LINENO "Missing ./${WORK_NAME}/${KERNEL_FILE}"
+	   exit -1
+	fi
+	
+	rm -rf ${KERNEL_DIR}
+	[ -d $KERNEL_DIR ] || mkdir $KERNEL_DIR
+	
+	tar xvf ./${WORK_NAME}/${KERNEL_FILE} -C ${KERNEL_DIR}  --strip-components=1
+fi
+
+if [[ "${PNAME}" == "origin" ]]; then		
+	TO_DO_PATCH="n"
+else
+	TO_DO_PATCH="y"
+fi
+
+if [[ "${TO_DO_PATCH}" == "y" ]]; then	
+    echo $LINENO "About to patch:  ${KERNEL_DIR}"
+
+	if [ ! -f "${PATCH_GZ_FILE}" ]; then
+	   echo $LINENO "Missing ${PATCH_GZ_FILE}"
+	   exit -1
+	else
+	echo $LINENO "Removing & Generating  ${PATCH_DIR}"
+	rm -rf ${PATCH_DIR}
+	[ -d $PATCH_DIR ] || mkdir $PATCH_DIR
+	    echo $LINENO "Tar the folder"
+		tar zxvf ${PATCH_GZ_FILE} -C ${PATCH_DIR}  --strip-components=1		
+	fi
+
+	if [ ! -f "${PATCH_LIST}" ]; then
+	   echo $LINENO "Missing ${PATCH_LIST}"
+	   exit -1
+	else
+	   echo $LINENO "Change Folder=${KERNEL_DIR}"
+	   cd ${KERNEL_DIR}
+	   for p in $(cat ${PATCH_LIST} );
+	    do
+	      echo "Patching::${p}"
+	      patch -p1 <  "${PATCH_DIR}/${p}"
+	    done
+	   cd ${PPWW}
+	fi
+fi
+
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # WorkForkernelPatch
 This is the platform to do the ONL Linux patch
+
+1. Reference
+Applying Patches To The Linux Kernel
+https://www.kernel.org/doc/html/v4.15/process/applying-patches.html
+
+
+2. Kernel source at pubic
+https://www.kernel.org/pub/linux/kernel/v4.x/
+
+3. Based Information for ONL.
+	3.1 Kernel source at the ONL folder:
+	./OpenNetworkLinux/packages/base/amd64/kernels/kernel-4.14-lts-x86-64-all/builds/jessie/linux-4.14.49
+
+	3.2. patch folder for specified kernerl.
+	./OpenNetworkLinux/packages/base/any/kernels/4.14-lts/patches
+
+	3.3 archives for kernel gar.zx source at ONL.
+	./OpenNetworkLinux/packages/base/any/kernels/archives
+
+4. How to use the script file to pull the specificed kernel source.
+	a. 3.7.10-Kernel.sh
+      => 1. This is the script file to pull the linux-3.7.10.tar.xz, utar the file to ./KERNEL/linux-3.7.10.
+	b. 3.7.10-Kernel-patch.sh
+      => 1. This is the script file to pull the linux-3.7.10.tar.xz, utar the file to ./KERNEL/linux-3.7.10-patches.
+	     2. To add the patches form ./patches-gz/patches-3.7.10.tar.gz
+    	
+5. How to make the patches-4.14.49.tar.gz
+
+5.1 Generate <file_name>.patch with diff
+    
+    a. make two folder a/ and b/, where b/ with the changes made from a/.	
+    b. the command is 
+	diff -Naur  a/ b/ >  ../0003-drivers-net-ethernet-broadcom-linux-4.16.18.patch
+	diff -Naur  a/ b/ >  ../0003-drivers-net-ethernet-broadcom-tg3-linux-4.16.18.patch 
+	diff -Naur  a/ b/ >  drivers-net-ethernet-broadcom-tg3-3.137k-version.patch                                         
+
+5.1 Generate <file_name>.patch with git diff
+
+   a. tar xvf linux-4.14.49.tar.xz
+   b. cd linux-4.14.49
+   c. git init
+   d. git add .
+   e. git commit -m "check in linux-4.14.49"
+      ==>merge the code from linux-4.14.67 drivers/net/ethernet/broadcom/tg3.c
+
+   f. To make patch from the last commit to the changes.
+      git diff > ../003-jcyu.patch
+      git diff > ../0003-drivers-net-ethernet-broadcom-linux-4.16.18.patch
+
+6. How to patch from patch-file
+ a. method-1
+  $ cat 0001-drivers-i2c-muxes-pca954x-deselect-on-exit.patch | patch  –p0
+  $ cat 0001-drivers-i2c-muxes-pca954x-deselect-on-exit.patch | patch  –p0
+ 
+ b. method-2 
+  $ patch -p1 < ../0001-drivers-i2c-muxes-pca954x-deselect-on-exit.patch
+  $ patch -p1 < ../0002-driver-support-intel-igb-bcm5461S-phy.patch
